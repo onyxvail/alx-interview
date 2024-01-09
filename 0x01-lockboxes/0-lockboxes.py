f8fadecbdb2e730c80d746a1c87ac859ee7084fb
onyxvail <ons.hfi@gmail.com>
2024-01-09 15:14:20 +0100

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""
-Module with the canUnlockAll function.
-"""
-
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened.
+    x = len(boxes)
+    item = [False] * x
+    item[0] = True
+    stack = [0]
 
-    Args:
-        boxes (list): A list of lists where
-        each box may contain keys to other boxes.
+    while stack:
+        box = stack.pop()
+        for each in boxes[box]:
+            if each >= 0 and each < x and not item[each]:
+                item[each] = True
+                stack.append(each)
 
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True
-    for index, box in enumerate(boxes):
-        if unlocked[index]:
-            for index, key in enumerate(box):
-                if key < len(unlocked):
-                    unlocked[key] = True
-                    for i in boxes[key]:
-                        unlocked[i] = True
-    return all(unlocked)
+    return all(item)
