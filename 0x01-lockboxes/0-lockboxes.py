bb0fc4c35a6d18ca40ede0a92363817d25aa6716
onyxvail <ons.hfi@gmail.com>
2024-01-09 15:06:30 +0100

    fixing error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,29 +15,13 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-    if not boxes or not isinstance(boxes, list):
-        return False
-
-    opened_boxes = set()
-    box_queue = [0]
-
-    while box_queue:
-        current_box = box_queue.pop(0)
-
-        for key in boxes[current_box]:
-            if 0 <= key < len(boxes) and key not in opened_boxes:
-                opened_boxes.add(key)
-                box_queue.append(key)
-
-    return len(opened_boxes) == len(boxes)
-
-
-if __name__ == "__main__":
-    boxes1 = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes1))
-
-    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes2))
-
-    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes3))
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    for index, box in enumerate(boxes):
+        if unlocked[index]:
+            for index, key in enumerate(box):
+                if key < len(unlocked):
+                    unlocked[key] = True
+                    for i in boxes[key]:
+                        unlocked[i] = True
+    return all(unlocked)
