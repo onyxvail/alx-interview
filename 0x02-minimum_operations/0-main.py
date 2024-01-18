b80c0821d5f39aba0b87ff6538f93a8a55921f81
onyxvail <ons.hfi@gmail.com>
2024-01-18 14:06:11 +0100

    fixing errors

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
-Main file for testing
+Main script for Minimum Operations module
 """
 
-minOperations = __import__('0-minoperations').minOperations
+min_operations = __import__('0-minoperations').min_operations
 
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+if __name__ == "__main__":
+    n1 = 4
+    print("Min # of operations to reach {} char: {}"
+          .format(n1, min_operations(n1)))
 
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    n2 = 12
+    print("Min # of operations to reach {} char: {}"
+          .format(n2, min_operations(n2)))
